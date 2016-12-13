c2fe0f7b6572db94b5bda5241fe8ae39ce74bfe5
8fprogmm11 <jon.sanchez@ikasle.egibide.org>
2016-12-13 21:52:37 +0100

    Cambios.

---
+++ lista_tareas/lista_tareas/mi_celda_view_cell.swift
@@ -0,0 +1,24 @@
+//
+//  mi_celda_view_cell.swift
+//  lista_tareas
+//
+//  Created by  on 13/12/16.
+//  Copyright © 2016 JonSanchez. All rights reserved.
+//
+
+import UIKit
+
+class mi_celda_view_cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- lista_tareas/lista_tareas/principal_TableViewController.swift
+++ lista_tareas/lista_tareas/principal_TableViewController.swift
@@ -29,17 +29,24 @@ class principal_TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "celda_tareas", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celda_tareas", for: indexPath) as! mi_celda_view_cell;
 
         // Configure the cell...
+        
+        
+        // Configure the cell...
+        
+        //cell.etiqueta.text = String(indexPath.row)
+        
+       // return cell
 
         return cell
     }
